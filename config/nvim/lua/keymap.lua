67410c42ccdf90515fc999a89aac1fe40a1d60b0
Cheezmeister <github@luchenlabs.com>
2024-06-15 11:15:51 -0400

    Nvim-only keymaps

---
+++ config/nvim/lua/keymap.lua
@@ -0,0 +1,11 @@
+-- Toggling
+vim.keymap.set('n', '<Leader>tn', '<Cmd>NvimTreeToggle<CR>')
+vim.keymap.set('n', '<Leader>to', '<Cmd>Outline<CR>')
+
+-- Exit insert mode
+vim.keymap.set('i', 'qk', '<Esc>')
+
+-- Intelligent Actions
+vim.keymap.set('n', '<Leader>if', '<Cmd>Lspsaga code_action<CR>')
+
+
